d16927f234b50a55399a42230a91101487243e4d
Luiza Mendes <luizaleitemendes@gmail.com>
2020-09-17 21:49:42 -0300

    Setting cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const controllerRegister = require("./controllers");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 // Controllers
 controllerRegister(app);
